cb6757ee1719d9b61871274991ba96468e0e47de
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-05-18 20:19:22 -0300

    update

--- tarea_2.hs
+++ tarea_2.hs
@@ -6,9 +6,21 @@ module Imp where
 
 import Prelude
 
---Ej1
---Ej2
---Ej3
+--Ej 1,2,3
+
+type Rama = (String,([String],Prog))
+type Mem = [(String,Val)] 
+type Prog = [Instr]
+
+data Val = ConstVal String [Val]
+	deriving Show
+data Instr = Asign [(String,Exp)] | Case String [Rama] | While String [Rama]
+	deriving Show
+data Exp = Var String | ConstExp String [Exp] 
+	deriving Show	
+
+--fn
+
 --Ej4
 --Ej5
 --Ej6
