434c2716616bb39690e16f2c5f24a8e5dae6b33e
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-05-18 22:19:28 -0300

    Finished 1,2,3

--- tarea_2.hs
+++ tarea_2.hs
@@ -32,11 +32,13 @@ update m ((x,v):a) = update (write m (x,v)) a
 write :: Mem -> (String,Val) -> Mem
 write [] (x,v) = [(x,v)]
 write ((y,u):a) (x,v) = case y == x of {
-	True -> ((x,v):a);
+	True -> (x,v):a;
 	False -> (y,u):(write a (x,v));
 }
 
---eval
+eval :: Mem -> Exp -> Val
+eval m (Var x) = lookupImp m x
+eval m (ConstExp c e) = ConstVal c (map (eval m) e)
 
 --Ej4
 --ejec 1 paso
