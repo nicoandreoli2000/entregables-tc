28882e852e8bf45ed3cb2ed3d826572a8bd73945
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-05-18 22:10:43 -0300

    update

--- tarea_2.hs
+++ tarea_2.hs
@@ -19,6 +19,23 @@ data Instr = Asign [(String,Exp)] | Case String [Rama] | While String [Rama]
 data Exp = Var String | ConstExp String [Exp] 
 	deriving Show	
 
+lookupImp :: Mem -> String -> Val
+lookupImp m x = case lookup x m of {
+	Just y -> y;
+	Nothing -> error "La variable no se encuentra en la memoria"
+}
+
+update :: Mem -> [(String,Val)] -> Mem
+update m [] = m
+update m ((x,v):a) = update (write m (x,v)) a  
+
+write :: Mem -> (String,Val) -> Mem
+write [] (x,v) = [(x,v)]
+write ((y,u):a) (x,v) = case y == x of {
+	True -> ((x,v):a);
+	False -> (y,u):(write a (x,v));
+}
+
 --eval
 
 --Ej4
