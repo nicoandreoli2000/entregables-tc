20aaf0f1bf26c5cb1909a444b5084ead4a32fc48
Nicolas Andreoli <nicolasandreoli2000@hotmail.com>
2022-05-29 21:20:58 -0300

    Feature: data and types

--- tarea_3.hs
+++ tarea_3.hs
@@ -5,13 +5,31 @@
 module Turing where
 
 --Ej1
+type Symbol = String
+blank :: Symbol
+blank = '#'
+type Tape = [Symbol]
 
 --Ej2
+data Action = Left Action | Right Action | Write Symbol Action
+                deriving Show
+type State = String
+init :: State
+init = 'i'
+halt :: State
+halt = 'h'
+type Code = [(State,Symbol,Action,State)]
 
 --Ej3
+type Config = (State,Tape)
 
 --Ej4
+-- step::Config->Config
 
 --Ej5
+-- exec::Code ->Tape->Tape
 
 --Ej6
+-- par
+-- shift-right
+-- reverse
